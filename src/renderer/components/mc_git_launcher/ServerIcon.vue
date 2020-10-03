977aa9dd57d694b12d9f5fff6f1d2b45e39f79f4
Bun133 <bun133.work@gmail.com>
2020-10-03 18:06:01 +0900

    add ServerIcon.vue

---
+++ src/renderer/components/mc_git_launcher/ServerIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <img src = "src"
+       class = "img"
+       alt = "Server Icon"/>
+</template>
+
+<script>
+export default {
+name: "ServerIcon",
+  props : {
+    src : {type:String,required:true}
+  }
+}
+</script>
+
+<style scoped>
+.img{
+  border-radius: 50%;
+}
+</style>
---
+++ src/renderer/pages/mc_git_launcher/Test.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-card
+      height=auto
+      width=100px
+      class="mx-auto"
+    >
+      <v-navigation-drawer permanent>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              MC Git Launcher
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list
+          dence
+          nav
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <ServerIcon src="../../static/vuetify-logo.svg"></ServerIcon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>
+                Test Server
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+      </v-navigation-drawer>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import ServerIcon from "../../components/mc_git_launcher/ServerIcon"
+export default {
+  name: "Test",
+  components: {ServerIcon}
+}
+</script>
+
+<style scoped>
+
+</style>
